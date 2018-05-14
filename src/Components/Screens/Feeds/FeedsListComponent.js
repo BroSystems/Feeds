a1e2fee9e68413803605e7f91eb5007f656597df
Molda <hbeygel@gmail.com>
2018-05-14 20:33:22 +0300

    FeedCell
    # created simple component
    # moved rendering of item from FeedsList to FeedCell

--- src/Components/Screens/Feeds/FeedsListComponent.js
+++ src/Components/Screens/Feeds/FeedsListComponent.js
@@ -12,46 +12,23 @@ import {
 
 } from 'react-native-ui-kitten';
 
-
-import Background from '../../../../Design/trees_feed.png';
+import FeedCell from './FeedCell';
 
 class FeedsListComponent extends Component {
 
     constructor() {
         super();
+        const Background = '../../../../Design/trees_feed.png';
+        
         this.feeds = [
-            {id: 0, name: 'feed 1'},
-            {id: 1, name: 'feed 2'},
-            {id: 2, name: 'feed 3'},
-            {id: 3, name: 'feed 4'},
-            {id: 4, name: 'feed 5'},
-            {id: 5, name: 'feed 6'},
-            {id: 6, name: 'feed 7'}
+            {id: 0, name: 'feed 1', image: Background},
+            {id: 1, name: 'feed 2', image: Background},
+            {id: 2, name: 'feed 3', image: Background},
+            {id: 3, name: 'feed 4', image: Background},
+            {id: 4, name: 'feed 5', image: Background},
+            {id: 5, name: 'feed 6', image: Background},
+            {id: 6, name: 'feed 7', image: Background}
         ];
-    }
-
-    renderFeed(item) {
-        console.log(item);
-        return (
-            <View 
-                key={item.item.id}
-                style={styles.item}>
-                <ImageBackground 
-                    style ={styles.image} 
-                    source={Background}
-                    resizeMode="stretch">
-                        <View style={styles.topTitles}>
-                            <Text style={styles.category}>FEED CATEGORY</Text>
-                            <Text style={styles.title}>{item.item.name}</Text>
-                        </View>
-                        <View style={styles.bottomTitles}>
-                            <Text style={styles.subtitle}>
-                                Inside the extraordinary world of Monument Valley 2
-                            </Text>
-                        </View>
-                </ImageBackground>
-            </View>
-        );
     }
 
     render() {
@@ -59,7 +36,7 @@ class FeedsListComponent extends Component {
             <View styles = {styles.container}>
                 <FlatList
                     data={this.feeds}
-                    renderItem={this.renderFeed.bind(this)}
+                    renderItem={item => FeedCell(item)}
                 />
             </View>
         );
@@ -73,60 +50,4 @@ const styles = {
         flex:1,
         width: '90%'
     },
-    item: {
-        margin: 12,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'relative',
-        shadowOffset: {width: 0,height: 12},
-        shadowOpacity: 0.16,
-        shadowColor: '#000000',
-        shadowRadius: 12,
-
-    },
-    image: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-        minHeight: 350,
-        borderRadius: 12,
-        borderWidth:1,
-        borderColor: 'transparent',
-        overflow: 'hidden',
-    },
-    topTitles: {
-        top:0,
-        margin: 24,
-    },
-    bottomTitles: {
-        bottom: 0,
-        position: 'absolute',
-        width: '100%',
-        padding: 24
-    },
-    category: {
-        backgroundColor: 'transparent',
-        fontSize: 15,
-        fontWeight: 'normal',
-        color: '#111111',
-        textAlign: 'left',
-        width: '100%'
-    },
-    title: {
-        backgroundColor: 'transparent',
-        fontSize: 28,
-        fontWeight: 'normal',
-        color: '#111111',
-        textAlign: 'left',
-        width: '100%'
-    },
-    subtitle: {
-        backgroundColor: 'transparent',
-        fontSize: 16,
-        fontWeight: 'normal',
-        color: '#111111',
-        textAlign: 'left',
-        width: '100%',
-    }
 };
