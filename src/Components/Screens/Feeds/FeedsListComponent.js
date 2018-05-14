7b69b73ae0e5ce4f24850031aa4de07ea25623fc
Molda <hbeygel@gmail.com>
2018-05-14 18:22:09 +0300

    FeedList
    # implemented style for Feed item

--- src/Components/Screens/Feeds/FeedsListComponent.js
+++ src/Components/Screens/Feeds/FeedsListComponent.js
@@ -4,12 +4,16 @@ import * as actions from '../../../Actions/FeedsActions';
 import {
     View,
     Text,
-    FlatList
+    FlatList,
+    ImageBackground
 } from 'react-native';
 
 import {
 
 } from 'react-native-ui-kitten';
+
+
+import Background from '../../../../Design/trees_feed.png';
 
 class FeedsListComponent extends Component {
 
@@ -29,12 +33,26 @@ class FeedsListComponent extends Component {
     renderFeed(item) {
         console.log(item);
         return (
-            <Text 
+            <View 
                 key={item.item.id}
-                style= {styles.item}>{item.item.name}</Text>
+                style={styles.item}>
+                <ImageBackground 
+                    style ={styles.image} 
+                    source={Background}
+                    resizeMode="stretch">
+                        <View style={styles.topTitles}>
+                            <Text style={styles.category}>FEED CATEGORY</Text>
+                            <Text style={styles.title}>{item.item.name}</Text>
+                        </View>
+                        <View style={styles.bottomTitles}>
+                            <Text style={styles.subtitle}>
+                                Inside the extraordinary world of Monument Valley 2
+                            </Text>
+                        </View>
+                </ImageBackground>
+            </View>
         );
     }
-
 
     render() {
         return (
@@ -53,11 +71,62 @@ export default connect(null, actions)(FeedsListComponent)
 const styles = {
     container: {
         flex:1,
-        paddingTop: 22,
+        width: '90%'
     },
     item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
+        margin: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        shadowOffset: {width: 0,height: 12},
+        shadowOpacity: 0.16,
+        shadowColor: '#000000',
+        shadowRadius: 12,
+
     },
+    image: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        minHeight: 350,
+        borderRadius: 12,
+        borderWidth:1,
+        borderColor: 'transparent',
+        overflow: 'hidden',
+    },
+    topTitles: {
+        top:0,
+        margin: 24,
+    },
+    bottomTitles: {
+        bottom: 0,
+        position: 'absolute',
+        width: '100%',
+        padding: 24
+    },
+    category: {
+        backgroundColor: 'transparent',
+        fontSize: 15,
+        fontWeight: 'normal',
+        color: '#111111',
+        textAlign: 'left',
+        width: '100%'
+    },
+    title: {
+        backgroundColor: 'transparent',
+        fontSize: 28,
+        fontWeight: 'normal',
+        color: '#111111',
+        textAlign: 'left',
+        width: '100%'
+    },
+    subtitle: {
+        backgroundColor: 'transparent',
+        fontSize: 16,
+        fontWeight: 'normal',
+        color: '#111111',
+        textAlign: 'left',
+        width: '100%',
+    }
 };
