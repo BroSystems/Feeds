5e0244c292736d32b18d6872d3a1c119f01a6f68
Molda <hbeygel@gmail.com>
2018-05-21 19:01:55 +0300

    Assets
    # created Images dictionary for managing static assets

---
+++ Client/Assets/images.js
@@ -0,0 +1,8 @@
+const images = {
+    btnLike: require('./Images/btn_like.png'),
+    btnLikeSelected: require('./Images/btn_like_selected.png'),
+    btnJoin: require('./Images/ic_join.png'),
+    btnJoinSelected: require('./Images/ic_join_selected.png')
+};
+
+export default images;
