8b3f7a54bd4439989349c6ad59ecd80cf29b6b7d
Molda <hbeygel@gmail.com>
2018-05-24 20:57:58 +0300

    LoginComponent
    # replaced ui-kitten with react-elements

--- Client/src/Components/Screens/Auth/LoginComponent.js
+++ Client/src/Components/Screens/Auth/LoginComponent.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
+  StyleSheet,
   View,
   Text
 } from 'react-native';
+
+import {
+  Button,
+  FormInput,
+  Card,
+} from 'react-native-elements';
 
 import * as actions from '../../../Actions/UserActions';
 
@@ -11,6 +18,11 @@ class LoginComponent extends Component {
 
   constructor() {
     super();
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onLoginPressed = this.onLoginPressed.bind(this);
+    this.onRegisterPressed = this.onRegisterPressed.bind(this);
+    this.errorType = this.errorType.bind(this);
   }
   
   onUsernameChange(text) {
@@ -31,13 +43,13 @@ class LoginComponent extends Component {
     this.props.registerNewUser(this.props);
   }
 
-  renderBottomPart() {
+  errorType = () => {
     if (this.props.isLogged) {
-      return (<RkText>Login Successfully!</RkText>)
+      return 'Login Successfully!'
     } else if (this.props.isRegistered) {
-      return (<RkText>Registered Successfully!</RkText>)
+      return 'Registered Successfully!'
     } else {
-      return (<RkText>enter your credentials in order to log in</RkText>)
+      return 'enter your credentials in order to log in';
     }
   }
 
@@ -47,45 +59,34 @@ class LoginComponent extends Component {
     }
     return (
       <View style={styles.container}>
-        <RkCard>
-          <RkTextInput 
-              placeholder='Email'
-              value= {this.props.username}
-              onChangeText={this.onUsernameChange.bind(this)}/>
-            <RkTextInput 
-              placeholder='Password'
-              value= {this.props.password}
-              onChangeText={this.onPasswordChange.bind(this)}
-              secureTextEntry={true}/>
-            {this.renderBottomPart()}
-        </RkCard>
-        <RkButton 
-            style={styles.button}
-            onPress={this.onLoginPressed.bind(this)}
-            enabled={false}>
-          Login
-        </RkButton>
-        <RkButton 
-            style={styles.button}
-            onPress={this.onRegisterPressed.bind(this)}>
-          Register
-        </RkButton>
+        <View styoe={styles.content}>
+            <FormInput 
+                placeholder='Email'
+                value= {this.props.username}
+                containerStyle={styles.fieldContainer}
+                inputStyle={{width:'100%'}}
+                onChangeText={this.onUsernameChange.bind(this)}/>
+              <FormInput 
+                placeholder='Password'
+                value= {this.props.password}
+                style={styles.input}
+                containerStyle={styles.fieldContainer}
+                inputStyle={{width:'100%'}}
+                onChangeText={this.onPasswordChange.bind(this)}
+                secureTextEntry={true}/>
+              <Text>{this.errorType()}</Text>
+          <Button 
+              style={{paddingVertical: 8,}}
+              buttonStyle={styles.button}
+              onPress={this.onLoginPressed.bind(this)}
+              title='Login'/>
+          <Button 
+              buttonStyle={styles.button}
+              onPress={this.onRegisterPressed.bind(this)}
+              title = 'Register'/>
+        </View>
       </View>
     );
-  }
-}
-
-const styles = {
-  container: {
-    display: 'flex',
-    padding: 12,
-    justifyContent: 'center',
-    backgroundColor: '#f1f1f1',
-    height: '100%'
-  },
-  button: {
-    alignSelf: 'center',
-    padding: 12,
   }
 }
 
@@ -101,3 +102,35 @@ const mapStateToProps = ({ user }) => {
 }
 
 export default connect(mapStateToProps, actions)(LoginComponent);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+    justifyContent: 'center',
+    alignContent: 'stretch',
+    backgroundColor: '#ffffff',
+    height: '100%'
+  },
+  content: {
+    flex:1,
+    backgroundColor: 'transparent'
+  },
+  input: {
+    paddingVertical: 4,
+  },
+  fieldContainer: {
+      backgroundColor: '#B8BCCC',
+      paddingHorizontal: 24,
+      paddingVertical: 4,
+      marginVertical: 8,
+  },
+  button: {
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 22,
+    height: 44,
+    paddingVertical: 4,
+    backgroundColor: '#C2CCB8'
+  }
+});
