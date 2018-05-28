7a76e22238877608d5496198bf0f63a406e89f71
Molda <hbeygel@gmail.com>
2018-05-28 17:48:27 +0300

    Package+Store
    # added redux-logger

--- Client/Store.js
+++ Client/Store.js
@@ -1,12 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducers from './src/Reducers';
 
 const store = createStore(
     reducers, // all the reducers
     {}, // app initial state
     compose(
-        applyMiddleware(thunk)
+        applyMiddleware(logger, thunk)
     )
 );
 
